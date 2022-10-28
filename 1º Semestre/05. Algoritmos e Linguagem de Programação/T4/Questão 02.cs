dbceaf5a5b1d876b57fa6ad22b689d6183de15eb
Gabriel Batista <109309661+BielBauer@users.noreply.github.com>
2022-10-28 18:59:30 -0300

    Quadrado dos valores de um array
    Programa na Linguagem C# que lê um conjunto de 10 números reais, armazenando-os em um array. O programa calcula o quadrado dos valores deste array, armazenando os resultados em outro array e imprime todos os conjuntos (arrays).

---
+++ 1º Semestre/05. Algoritmos e Linguagem de Programação/T4/Questão 02.cs
@@ -0,0 +1,36 @@
+{
+// 2) Escrever um programa com a Linguagem C# para ler um conjunto de números reais, armazenando-os em um array.
+// O programa deverá calcular o quadrado dos valores deste array, armazenando os resultados em outro array.
+// Os conjuntos têm 10 elementos cada. Imprimir todos os conjuntos (arrays).
+
+
+            // Definição de Variáveis e arrays e entrada de valores no primeiro array:
+
+            int i;
+            int[] numeros = new int[10];
+            int[] quadrados = new int[10];
+            
+            Console.WriteLine("Entre com 10 valores numéricos inteiros: ");
+
+            for (i = 0; i < 10; i++)
+                numeros[i] = int.Parse(Console.ReadLine());
+
+// Exibição dos números do array:
+
+            Console.WriteLine("Os valores armazenados no array foram: ");
+
+            for (i = 0; i < 10; i++)
+                Console.WriteLine(numeros[i]);
+
+// Cálculo dos quadrados e exibição do segundo array:
+
+            Console.WriteLine("Os quadrados dos valores armazenados no array são: ");
+
+            for (i = 0; i < 10; i++)
+            {
+                quadrados[i] = numeros[i] *= numeros[i];
+                Console.WriteLine(quadrados[i]);
+            }
+                        
+            Console.ReadKey();
+}
