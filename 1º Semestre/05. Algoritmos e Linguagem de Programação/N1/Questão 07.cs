e2b3891a94e99e51055e948d2b050197d0ab7c9a
Gabriel Batista <109309661+BielBauer@users.noreply.github.com>
2022-11-04 19:00:21 -0300

    Questão 07
    Programa na Linguagem C# que exiba somente os números ímpares existentes entre 2000 e 9000.

--- 1º Semestre/05. Algoritmos e Linguagem de Programação/N1/Questão 07.cs
+++ 1º Semestre/05. Algoritmos e Linguagem de Programação/N1/Questão 07.cs
@@ -1,19 +1,16 @@
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int x = 2000;
+\\ 7. Escreva um programa na Linguagem C# que exiba somente os números ímpares existentes entre 2000 e 9000.
 
-            while (x < 9000)
-                if (x % 2 != 0)
-                {
-                    Console.WriteLine(x);
-                    x++;
-                }
-                else
-                    x++;
-            Console.ReadKey();              
-        }
+{
+int x = 2000;
+
+while (x < 9000)
+    if (x % 2 != 0)
+    {
+        Console.WriteLine(x);
+        x++;
     }
+    else
+        x++;
+    
+    Console.ReadKey();              
 }
