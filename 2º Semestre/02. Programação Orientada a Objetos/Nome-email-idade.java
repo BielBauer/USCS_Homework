00711772fe81ec7a3e904e5e31eadb394c9f95a2
Gabriel Batista <109309661+BielBauer@users.noreply.github.com>
2023-02-14 20:49:30 -0300

    Nome-email-idade
    Testes iniciais com o IDLE NetBeans

---
+++ 2º Semestre/02. Programação Orientada a Objetos/Nome-email-idade.java
@@ -0,0 +1,11 @@
+{
+        Pessoa pl;
+        pl = new Pessoa ();
+        pl.nome = "Lucca";
+        pl.email = "lucc@uol.com.br";
+        pl.idade = 19;
+        
+        System.out.println(pl.nome);
+        System.out.println(pl.email);
+        System.out.println(pl.idade);
+}
