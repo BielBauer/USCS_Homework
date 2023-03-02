ad8118a0d7a2a4b80e150058612deb0e0f91bad3
Gabriel Batista <109309661+BielBauer@users.noreply.github.com>
2023-03-02 20:17:57 -0300

    Diagrama de Blocos

---
+++ 2º Semestre/04. Programação Avançada/Diagrama de Blocos 01.c
@@ -0,0 +1,19 @@
+![Questão 01](https://user-images.githubusercontent.com/109309661/222584660-d11a377c-8427-46da-99d9-66c46777699d.jpg)
+
+#include <stdio.h>
+
+int main()
+
+{
+	int C = 0;
+	int S = 10;
+	
+	while (C < 5)
+		C = C + 1;
+	
+	S = S + C;
+	
+	printf("%d", S);
+
+	return 0;
+}
