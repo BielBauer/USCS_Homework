757745b4237767843425295a1c23b18ffeecc915
Gabriel Batista <109309661+BielBauer@users.noreply.github.com>
2024-02-29 22:10:16 -0300

    Create Média_Aritimética.dart

---
+++ 4º Semestre/5. Desenvolvimento Mobile/Média_Aritimética.dart
@@ -0,0 +1,15 @@
+// 1. Calculadora de Média:
+// Crie um programa que recebe três notas e calcula a média aritmética. Mostre o resultado na tela.
+
+void main() {
+
+double n1, n2, n3, media;
+
+n1 = 7.0;
+n2 = 8.5;
+n3 = 8.0;
+media = (n1+n2+n3)/3;
+
+print("A média das notas inseridas é: $media");
+
+}
