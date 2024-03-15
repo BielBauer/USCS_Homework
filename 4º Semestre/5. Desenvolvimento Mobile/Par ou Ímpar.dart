6e6b56f36419f57c85d789da9d49f982e01853f5
Gabriel Batista <109309661+BielBauer@users.noreply.github.com>
2024-03-14 22:03:00 -0300

    Par ou Ímpar.dart

---
+++ 4º Semestre/5. Desenvolvimento Mobile/Par ou Ímpar.dart
@@ -0,0 +1,20 @@
+// 3. Verificação de Número Par ou Ímpar:
+// Escreva um programa que verifica se um número é par ou ímpar.
+
+void main()
+{
+  int x;
+  x = 108;
+  
+if (x % 2 == 0)
+  
+  {
+    print('O número registrado é par.');
+  }
+  
+else
+  
+  {
+    print('O número registrado é ímpar.');
+  }
+}
