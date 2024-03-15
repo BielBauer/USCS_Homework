e9a412138149468a8167e8ec0e1b87e5c2fe37f7
Gabriel Batista <109309661+BielBauer@users.noreply.github.com>
2024-03-14 21:50:19 -0300

    Conversão de Temperatura - Celsius para Fahrenheit.dart

---
+++ 4º Semestre/5. Desenvolvimento Mobile/Conversão de Temperatura - Celsius para Fahrenheit.dart
@@ -0,0 +1,13 @@
+// 2. Conversão de Temperatura:
+// Faça um programa que converte uma temperatura em Celsius para Fahrenheit.
+// A fórmula é: F = (C * 9/5) + 32.
+
+void main()
+{
+  double C, F;
+  C = 34.0;
+  F = (C * 9/5) + 32;
+  {
+    print('A Temperatura correspondente em Fahrenheit é: $FºF.');
+  }
+}
